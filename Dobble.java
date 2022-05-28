ff3f045a114f1904e6c09930a872cad11f254694
Bastián Escribano <bastian.escribano@usach.cl>
2022-05-27 23:30:14 -0400

    270522

---
+++ Dobble.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Dobble {
+
+    // Mazo de Cartas
+
+    private Card carta;
+    private int maxC;
+
+    public Dobble(Card carta, int maxC){
+        this.carta = carta;
+        this.maxC = maxC;
+    }
+
+
+
+
+}
