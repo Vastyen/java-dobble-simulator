9eb2f279369c3681fb263738684c23656ee11520
Bastián Escribano <bastian.escribano@usach.cl>
2022-06-16 23:13:37 -0400

    1606: Menú

--- src/main/java/Dobble.java
+++ src/main/java/Dobble.java
@@ -40,8 +40,43 @@ public class Dobble {
         return totalElements;
     }
 
-    //public ArrayList missingCards(ArrayList cardsDeck){
-//    }
+    public ArrayList missingCards(ArrayList cardsDeck){
+
+
+        ArrayList newCardsDeck = new ArrayList();
+
+        Card firstCard = this.cardsDeck.get(1);
+        int sizeFirstCard = firstCard.getListElements().size();
+
+        int sizeDeck = cardsDeck.size();
+
+        int order = sizeFirstCard-1;
+        int totalElements = order*order+order+1;
+
+        if (esPrimo(order)){
+            return cardsDeck;
+        }
+
+        else {
+            return newCardsDeck;
+        }
+    }
+
+
+    public static boolean esPrimo(int numero) {
+        // El 0, 1 y 4 no son primos
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+            // Si es divisible por cualquiera de estos números, no
+            // es primo
+            if (numero % x == 0)
+                return false;
+        }
+        // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+        return true;
+    }
 
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -22,46 +22,73 @@ public class main {
             opt = sc.nextInt();
         } while (opt != 1 && opt != 2 && opt != 3 && opt != 4);
 
-        int n = 7;
 
-        ArrayList<Integer> card = new ArrayList();
-        Dobble mazo = new Dobble();
+        Player jugador = new Player(startUser, 0, false);
+        ArrayList<Player> gamePlayers = new ArrayList();
+        gamePlayers.add(jugador);
+        ArrayList cardsSet = new ArrayList<>();
+        ArrayList<String> gameArea = new ArrayList();
 
-        for (int i = 1; i <= n + 1; i++) {
-            card.add(i);
-        }
-        mazo.addCard(card);
-        for (int j = 1; j <= n; j++) {
-            card.clear();
-            card.add(1);
-            for (int k = 1; k <= n; k++) {
-                card.add(n * j + (k + 1));
+        DobbleGame game = new DobbleGame(cardsSet, gameArea, gamePlayers, 1, "No hay juegos activos.");
+
+        if (opt == 1) {
+
+            System.out.println("Introduzca la cantidad de elementos de la baraja");
+            int cantEle = sc.nextInt();
+            int n = cantEle - 1;
+
+            ArrayList<Integer> card = new ArrayList();
+            Dobble mazo = new Dobble();
+
+            for (int i = 1; i <= n + 1; i++) {
+                card.add(i);
             }
             mazo.addCard(card);
-        }
-        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 card.clear();
-                card.add(i + 1);
-
+                card.add(1);
                 for (int k = 1; k <= n; k++) {
-                    card.add(n + 2 + n * (k - 1) + (((i - 1) * (k - 1) + j - 1) % n));
+                    card.add(n * j + (k + 1));
                 }
                 mazo.addCard(card);
             }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    card.clear();
+                    card.add(i + 1);
+
+                    for (int k = 1; k <= n; k++) {
+                        card.add(n + 2 + n * (k - 1) + (((i - 1) * (k - 1) + j - 1) % n));
+                    }
+                    mazo.addCard(card);
+                }
+            }
+
+            cardsSet = mazo.getCardsDeck();
+
+            System.out.println("Introduzca el modo de juego");
+            String gameMode = sc.next();
+
+            game.setGameDeck(cardsSet);
+            game.setGameMode(gameMode);
         }
 
-        ArrayList cardsSet = mazo.getCardsDeck();
 
-        int index = 3;
+        if (opt == 2) {
+            System.out.println("Introduzca el nombre del nuevo jugador");
+            String newPlayer = sc.next();
+            Player newPlayerGame = new Player(newPlayer, 0, false);
+            game.getGamePlayers().add(newPlayerGame);
+            game.setGameNumPlayers(game.getGameNumPlayers() + 1);
+            System.out.println(game.getGamePlayers());
+            System.out.println(game.getGameNumPlayers());
+        }
 
-        Card nthCard = mazo.nthCard(cardsSet,  index);
 
-        System.out.println(nthCard.getListElements());
 
-        System.out.println(mazo.findTotalCards(nthCard));
-        System.out.println(mazo.requiredElements(nthCard));
 
-    }
 
-}
+
+
+
+    }}
